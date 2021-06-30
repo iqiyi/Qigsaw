218b9a3a50084579f8c03e2f32361710b566c3a8
kuangzhongwen <kuangzhongwen@58.com>
2021-06-30 20:47:59 +0800

    修复宿主 app 应用 resguard 插件后导致分包的 ABI APK 包大小增加问题

--- buildSrc/src/main/groovy/com/iqiyi/qigsaw/buildtool/gradle/QigsawAppBasePlugin.groovy
+++ buildSrc/src/main/groovy/com/iqiyi/qigsaw/buildtool/gradle/QigsawAppBasePlugin.groovy
@@ -199,8 +199,19 @@ class QigsawAppBasePlugin extends QigsawPlugin {
                         splitBaseApkForABIs.packageAppDir = packageAppDir
                         splitBaseApkForABIs.baseApksDir = baseApksDir
                         splitBaseApkForABIs.unzipBaseApkDir = unzipBaseApkDir
-                        baseAssemble.dependsOn splitBaseApkForABIs
-                        packageApp.finalizedBy splitBaseApkForABIs
+                        Task resguardTask = AGPCompat.getResguardTask(project, "${baseVariant.name.capitalize()}")
+                        // apply plugin: 'AndResGuard' 必须在 qigsaw application 插件后应用，否则会查找不到 resguardTask
+                        if (resguardTask != null) {
+                            SplitLogger.w("found resguardTask")
+                            resguardTask.dependsOn baseAssemble
+                            baseAssemble.finalizedBy resguardTask
+                            splitBaseApkForABIs.dependsOn resguardTask
+                            resguardTask.finalizedBy splitBaseApkForABIs
+                        } else {
+                            SplitLogger.w("not found resguardTask")
+                            baseAssemble.dependsOn splitBaseApkForABIs
+                            packageApp.finalizedBy splitBaseApkForABIs
+                        }
                     }
 
                     //for supporting split content-provider
